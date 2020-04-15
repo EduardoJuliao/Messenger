0cb1047f5af7aa727f4c9f49e503199a3e050a81
Sidney <sidney.bertolini@gmail.com>
2020-04-15 19:54:38 -0300

    my checks

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import http from 'http';
+import socketio from 'socket.io';
+
+const requestListener = function (_: any, res: any) {
+  res.writeHead(200);
+  res.end(
+    '<!DOCTYPE html>Hello, World! <script src="/socket.io/socket.io.js"></script><script>var socket = io();</script>'
+  );
+};
+
+const server = http.createServer(requestListener);
+server.listen(3000);
+
+const io = socketio(server);
+
+io.on('connection', () => {
+  console.log('New conn');
+});
