36e62a915fba7bff550da5eb7b4a4eed05bb1eb5
Sidney <sidney.bertolini@gmail.com>
2020-04-15 20:32:43 -0300

    looking good looking fresh

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import socketio from 'socket.io';
 const requestListener = function (_: any, res: any) {
   res.writeHead(200);
   res.end(
-    '<!DOCTYPE html>Hello, World! <script src="/socket.io/socket.io.js"></script><script>var socket = io();</script>'
+    '<!DOCTYPE html>Hello, World! <script src="/socket.io/socket.io.js"></script><script>var socket = io();socket.on("message", (message) => {console.log(message);});</script>'
   );
 };
 
@@ -13,6 +13,7 @@ server.listen(3000);
 
 const io = socketio(server);
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
   console.log('New conn');
+  socket.emit('message', 'fuck you lmao');
 });
